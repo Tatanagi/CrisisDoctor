d7ad1bd392790d286b0cb71be5304883d9a9dd56
Tatanagi <kayrafaelchester1925@gmail.com>
2025-02-12 03:59:41 +0800

    Added Ammo And Ammo UI

--- Assets/Scripts/Kyle/Gun/PlayerShoot.cs
+++ Assets/Scripts/Kyle/Gun/PlayerShoot.cs
@@ -2,29 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Rendering;
+using TMPro;
 
 public class PlayerShoot : MonoBehaviour
 {
     [SerializeField]
     private GameObject _bulletPrefab;
-
     [SerializeField]
     private float _bulletSpeed;
-
     [SerializeField]
     private Transform _gunOffset;
-
     [SerializeField]
     private float _timeBetweenShots;
-
+    [SerializeField]
+    private int _maxAmmo = 10; // Maximum ammo count
+    private int _currentAmmo;
+    
+    [SerializeField]
+    private TMP_Text _ammoText; // Reference to a UI TextMeshPro element to display ammo
+    
     private bool _fireSingle;
     private bool _fireContinuously;
     private float _lastFireTime;
 
+    void Start()
+    {
+        _currentAmmo = _maxAmmo;
+        UpdateAmmoUI();
+    }
+
     void Update()
     {
-        if (_fireContinuously || _fireSingle)
+        if ((_fireContinuously || _fireSingle) && _currentAmmo > 0)
         {
             float timeSinceLastFire = Time.time - _lastFireTime;
 
@@ -39,12 +48,18 @@ public class PlayerShoot : MonoBehaviour
 
     private void FireBullet()
     {
-        GameObject bullet = Instantiate(_bulletPrefab, _gunOffset.position, _gunOffset.rotation);
-        Rigidbody2D rigidbody = bullet.GetComponent<Rigidbody2D>();
-
-        if (rigidbody != null)
+        if (_currentAmmo > 0)
         {
-            rigidbody.velocity = _gunOffset.up * _bulletSpeed;
+            GameObject bullet = Instantiate(_bulletPrefab, _gunOffset.position, _gunOffset.rotation);
+            Rigidbody2D rigidbody = bullet.GetComponent<Rigidbody2D>();
+
+            if (rigidbody != null)
+            {
+                rigidbody.velocity = _gunOffset.up * _bulletSpeed;
+            }
+
+            _currentAmmo--;
+            UpdateAmmoUI();
         }
     }
 
@@ -55,6 +70,20 @@ public class PlayerShoot : MonoBehaviour
         if (inputValue.isPressed)
         {
             _fireSingle = true;
+        }
+    }
+
+    public void Reload(int ammoAmount)
+    {
+        _currentAmmo = Mathf.Min(_currentAmmo + ammoAmount, _maxAmmo);
+        UpdateAmmoUI();
+    }
+
+    private void UpdateAmmoUI()
+    {
+        if (_ammoText != null)
+        {
+            _ammoText.text = _currentAmmo + "/" + _maxAmmo;
         }
     }
 }
