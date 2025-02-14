6608fa103145d7aec10d5455637b5dac2dff05ea
Tatanagi <kayrafaelchester1925@gmail.com>
2025-02-14 23:38:17 +0800

    Added Player Health

--- Assets/Scripts/Kyle/Player/PlayerHealth.cs
+++ Assets/Scripts/Kyle/Player/PlayerHealth.cs
@@ -10,9 +10,24 @@ public class PlayerHealth : MonoBehaviour
 
     public int maxHealth = 100;
     public int currentHealth;
-
+    public HealthBar healthBar;
     void Start()
     {
         currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            TakeDamage(20);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
     }
 }
