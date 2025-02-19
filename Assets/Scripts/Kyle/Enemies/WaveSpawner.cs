7ec57730427b74e03a7cd4877d69925acfec1ff7
Tatanagi <kayrafaelchester1925@gmail.com>
2025-02-19 22:17:22 +0800

    WaveSpawanners

--- Assets/Scripts/Kyle/Enemies/WaveSpawner.cs
+++ Assets/Scripts/Kyle/Enemies/WaveSpawner.cs
@@ -1,39 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class WaveSpawner : MonoBehaviour
 {
+    public TextMeshProUGUI waveCountText;
     public float spawnRate = 1.0f;
     public float timeBetweenWaves = 3.0f;
-
     public int enemyCount;
+    public GameObject enemy;
+    public Transform[] spawnPoints; // Array of spawn points
 
-    public GameObject enemy; 
-
-    bool waveIsDone = true;
+    private int waveCount = 1;
+    private bool waveIsDone = true;
 
     void Update()
     {
-        if (waveIsDone == true)
+        waveCountText.text = waveCount.ToString();
+        if (waveIsDone)
         {
-            StartCoroutine(waveSpawner());
-        }    
+            StartCoroutine(WaveSpawnerCoroutine());
+        }
     }
 
-    IEnumerator waveSpawner()
+    IEnumerator WaveSpawnerCoroutine()
     {
         waveIsDone = false;
-        
+
         for (int i = 0; i < enemyCount; i++)
         {
-            GameObject enemyClone = Instantiate(enemy);
+            Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)]; // Select a random spawn point
+            Instantiate(enemy, spawnPoint.position, spawnPoint.rotation);
             yield return new WaitForSeconds(spawnRate);
         }
-        
-        spawnRate -= 0.1f;
+
+        spawnRate = Mathf.Max(0.1f, spawnRate - 0.1f); // Prevent spawnRate from becoming too low
+        enemyCount += 3;
+        waveCount += 1;
+
         yield return new WaitForSeconds(timeBetweenWaves);
-        
         waveIsDone = true;
     }
 }
