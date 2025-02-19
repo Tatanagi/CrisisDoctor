d5aadaeb76381fcfa105477e2abdf5ffaa2ab8ca
Tatanagi <kayrafaelchester1925@gmail.com>
2025-02-19 21:44:15 +0800

    Implemented The Ammo UI and Waves

---
+++ Assets/Scripts/Kyle/Enemies/WaveSpawner.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public float spawnRate = 1.0f;
+    public float timeBetweenWaves = 3.0f;
+
+    public int enemyCount;
+
+    public GameObject enemy; 
+
+    bool waveIsDone = true;
+
+    void Update()
+    {
+        if (waveIsDone == true)
+        {
+            StartCoroutine(waveSpawner());
+        }    
+    }
+
+    IEnumerator waveSpawner()
+    {
+        waveIsDone = false;
+        
+        for (int i = 0; i < enemyCount; i++)
+        {
+            GameObject enemyClone = Instantiate(enemy);
+            yield return new WaitForSeconds(spawnRate);
+        }
+        
+        spawnRate -= 0.1f;
+        yield return new WaitForSeconds(timeBetweenWaves);
+        
+        waveIsDone = true;
+    }
+}
